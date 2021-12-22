d00aacfd918d78e9670a6f27469a33ecd024d0a9
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2021-12-22 22:47:06 +0300

    storage step

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -60,11 +60,9 @@ class FileStorage:
 
     def delete(self, obj=None):
         """delete obj from __objects if it’s inside"""
-        if obj is not None:
-            del self.__objects['{}.{}'.format(obj.__class__.__name__, obj.id)]
-            # key = obj.__class__.__name__ + '.' + obj.id
-            # if key in self.__objects:
-            #     del self.__objects[key]
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in self.__objects:
+                del self.__objects[key]
 
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
